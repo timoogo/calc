097b8075536948f7c44c0eb1c26e157e51e531c0
Timothée Gaultier <timothee.gaultier@gmail.com>
2021-04-09 08:05:01 +0200

    react

--- App.js
+++ App.js
@@ -1,21 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {
+  Component
+} from 'react';
+import {
+  View,
+  Text,
+  AppRegistry
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Style from './src/Style';
+import InputButton from './src/InputButton';
+
+// Define the input buttons that will be displayed in the calculator.
+const inputButtons = [
+    [1, 2, 3, '/'],
+    [4, 5, 6, '*'],
+    [7, 8, 9, '-'],
+    [0, '.', '=', '+']
+];
+
+class ReactCalculator extends Component {
+
+    render() {
+        return (
+            <View style={Style.rootContainer}>
+                <View style={Style.displayContainer}></View>
+                <View style={Style.inputContainer}>
+                    {this._renderInputButtons()}
+                </View>
+            </View>
+        )
+    }
+
+    _renderInputButtons() {
+        let views = [];
+
+        for (var r = 0; r < inputButtons.length; r ++) {
+            let row = inputButtons[r];
+
+            let inputRow = [];
+            for (var i = 0; i < row.length; i ++) {
+                let input = row[i];
+
+                inputRow.push(
+                    <InputButton
+                        value={input}
+                        onPress={this._onInputButtonPressed.bind(input)}
+                        key={r + "-" + i}/>
+                );
+            }
+
+            views.push(<View style={Style.inputRow} key={"row-" + r}>{inputRow}</View>)
+        }
+
+        return views;
+    }
+    _onInputButtonPressed(input) {
+        alert(input)
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default ReactCalculator
---
+++ src/InputButton.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import {
+    View,
+    Text
+} from 'react-native';
+import TouchableHighlight from './TouchableHighlight';
+import Style from './Style';
+
+
+
+ class InputButton extends Component {
+
+    render() {
+        return (
+            <TouchableHighlight style={Style.inputButton}
+                                underlayColor="#193441"
+                                onPress={this.props.onPress}>
+                <Text style={Style.inputButtonText}>{this.props.value}</Text>
+            </TouchableHighlight>
+        )
+    }
+
+}
+export default InputButton

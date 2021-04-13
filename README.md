4651a0a723885092217573e563432054f9b4713d
Timothée Gaultier <timothee.gaultier@gmail.com>
2021-04-13 22:10:59 +0200

    calc react

--- README.md
+++ README.md
@@ -8,4 +8,4 @@ N'ayant pas un ordinateur très puissant (8gb de RAM), je n'ai pas pu faire gran
 J'avais essayé d'avancer un maximum sur [dartpad](dartpad.dev) mais je n'ai pas compris certains points.
 
 ## calc-react
-J'ai essayé d'avancer un maximum, mais pour des raisons qui m'échape, mon serveur local prennait énormément de temps pour afficher les changements (des fois, jusqu'a 10 min d'attente)
+J'ai essayé d'avancer un maximum, mais pour des raisons qui m'échape, mon serveur local prennait énormément de temps pour affichdcder les changements (des fois, jusqu'a 10 min d'attente)
---
+++ calc-react/App.js
@@ -0,0 +1,115 @@
+import React, {
+    Component
+} from 'react';
+import {
+    View,
+    Text,
+    AppRegistry
+} from 'react-native';
+import Style from './src/Style';
+import InputButton from "./src/InputButton";
+
+const inputButtons = [
+    [1, 2, 3, '/'],
+    [4, 5, 6, '*'],
+    [7, 8, 9, '-'],
+    [0, '.', '=', '+']
+];
+
+class ReactCalculator extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            inputValue: 0
+        }
+    }
+    render() {
+        return (
+            <View style={Style.rootContainer}>
+                <View style={Style.displayContainer}>
+                    <Text style={Style.displayText}>{this.state.inputValue}</Text>
+                </View>
+                <View style={Style.inputContainer}>
+                    {this._renderInputButtons()}
+                </View>
+            </View>
+        )
+    }
+
+    _renderInputButtons() {
+        let views = [];
+
+        for (var r = 0; r < inputButtons.length; r ++) {
+            let row = inputButtons[r];
+
+            let inputRow = [];
+            for (var i = 0; i < row.length; i ++) {
+                let input = row[i];
+
+                inputRow.push(
+                    <InputButton
+                        value={input}
+                        onPress={this._onInputButtonPressed.bind(this, input)}
+                        key={r + "-" + i}
+                    />
+                );
+            }
+
+            views.push(<View style={Style.inputRow} key={"row-" + r}>{inputRow}</View>)
+        }
+
+        return views;
+    }
+    _onInputButtonPressed(input) {
+        switch (typeof input) {
+            case 'number':
+                return this._handleNumberInput(input);
+            case 'string':
+                return this._handleStringInput(input)
+        }
+    }
+    _handleNumberInput(num) {
+        let inputValue = (this.state.inputValue * 10) + num;
+
+        this.setState({
+            inputValue: inputValue
+        })
+    }
+    _handleStringInput(str) {
+        switch (str) {
+            case '/':
+            case '*':
+            case '+':
+            case '-':
+
+                this.setState({
+                    selectedSymbol: str,
+                    previousInputValue: this.state.inputValue,
+                    inputValue: 0
+
+                });
+
+            case '=':
+                let symbol = this.state.selectedSymbol,
+                    inputValue = this.state.inputValue,
+                    previousInputValue = this.state.previousInputValue;
+
+                if (!symbol) {
+                    return;
+                }
+
+                this.setState({
+                    previousInputValue: 0,
+                    inputValue: eval(previousInputValue + symbol + inputValue),
+                    selectedSymbol: null
+                });
+                break;
+        }
+    }
+
+
+
+}
+
+export default ReactCalculator
